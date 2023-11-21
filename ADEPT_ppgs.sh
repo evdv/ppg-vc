817f4f85345e214ffebd6c5305f0343bb8ccc344
evdv <emelie.vdvreken@gmail.com>
2023-11-21 16:16:05 +0000

    Scripts for repeated ppg computation

---
+++ ADEPT_ppgs.sh
@@ -0,0 +1,7 @@
+python 1_compute_ctc_att_bnf.py --output_dir /media/emelie/EMELIESSD1/ppgs/ADEPT_VAD/beginning/ --wav_dir /media/emelie/EMELIESSD1/ADEPT/wav_22khz/topical_emphasis/vad_output/beginning/
+python 1_compute_ctc_att_bnf.py --output_dir /media/emelie/EMELIESSD1/ppgs/ADEPT_VAD/middle/ --wav_dir /media/emelie/EMELIESSD1/ADEPT/wav_22khz/topical_emphasis/vad_output/middle/
+python 1_compute_ctc_att_bnf.py --output_dir /media/emelie/EMELIESSD1/ppgs/ADEPT_VAD/end/ --wav_dir /media/emelie/EMELIESSD1/ADEPT/wav_22khz/topical_emphasis/vad_output/end/
+
+python 1_compute_ctc_att_bnf.py --output_dir /media/emelie/EMELIESSD1/ppgs/ADEPT_NO_VAD/beginning/ --wav_dir /media/emelie/EMELIESSD1/ADEPT/wav_22khz/topical_emphasis/beginning/
+python 1_compute_ctc_att_bnf.py --output_dir /media/emelie/EMELIESSD1/ppgs/ADEPT_NO_VAD/middle/ --wav_dir /media/emelie/EMELIESSD1/ADEPT/wav_22khz/topical_emphasis/middle/
+python 1_compute_ctc_att_bnf.py --output_dir /media/emelie/EMELIESSD1/ppgs/ADEPT_NO_VAD/end/ --wav_dir /media/emelie/EMELIESSD1/ADEPT/wav_22khz/topical_emphasis/end/
---
+++ LJ_MELDJ_ppgs.sh
@@ -0,0 +1,4 @@
+python 1_compute_ctc_att_bnf.py --output_dir /media/emelie/EMELIESSD1/ppgs/LJ_MFA/ADEPT/ --wav_dir /media/emelie/EMELIESSD1/puppeteering/LJ/ARPA/ADEPT/no_reference/
+python 1_compute_ctc_att_bnf.py --output_dir /media/emelie/EMELIESSD1/ppgs/LJ_MFA/LJ_test_30/ --wav_dir /media/emelie/EMELIESSD1/puppeteering/LJ/ARPA/LJ_test_30/no_reference/
+python 1_compute_ctc_att_bnf.py --output_dir /media/emelie/EMELIESSD1/ppgs/MELDJ_MFA/ADEPT/ --wav_dir /media/emelie/EMELIESSD1/puppeteering/meLdJ/ARPA/ADEPT/no_reference/
+python 1_compute_ctc_att_bnf.py --output_dir /media/emelie/EMELIESSD1/ppgs/MELDJ_MFA/LJ_test_30/ --wav_dir /media/emelie/EMELIESSD1/puppeteering/meLdJ/ARPA/LJ_test_30/no_reference/
