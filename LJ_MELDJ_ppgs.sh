1e2003bb109dc792c45eddad0f016996be13c5fb
evdv <emelie.vdvreken@gmail.com>
2023-11-23 17:38:53 +0000

    More complete scripts, LJ/MELDJ inference now exists for all 6 ADEPT inference tsv files

--- LJ_MELDJ_ppgs.sh
+++ LJ_MELDJ_ppgs.sh
@@ -1,4 +1,21 @@
-python 1_compute_ctc_att_bnf.py --output_dir /media/emelie/EMELIESSD1/ppgs/LJ_MFA/ADEPT/ --wav_dir /media/emelie/EMELIESSD1/puppeteering/LJ/ARPA/ADEPT/no_reference/
 python 1_compute_ctc_att_bnf.py --output_dir /media/emelie/EMELIESSD1/ppgs/LJ_MFA/LJ_test_30/ --wav_dir /media/emelie/EMELIESSD1/puppeteering/LJ/ARPA/LJ_test_30/no_reference/
-python 1_compute_ctc_att_bnf.py --output_dir /media/emelie/EMELIESSD1/ppgs/MELDJ_MFA/ADEPT/ --wav_dir /media/emelie/EMELIESSD1/puppeteering/meLdJ/ARPA/ADEPT/no_reference/
 python 1_compute_ctc_att_bnf.py --output_dir /media/emelie/EMELIESSD1/ppgs/MELDJ_MFA/LJ_test_30/ --wav_dir /media/emelie/EMELIESSD1/puppeteering/meLdJ/ARPA/LJ_test_30/no_reference/
+
+# No VAD
+python 1_compute_ctc_att_bnf.py --output_dir /media/emelie/EMELIESSD1/ppgs/LJ_MFA/ADEPT_NO_VAD/beginning/ --wav_dir /media/emelie/EMELIESSD1/puppeteering/LJ/ARPA/ADEPT_NO_VAD/no_reference/beginning/
+python 1_compute_ctc_att_bnf.py --output_dir /media/emelie/EMELIESSD1/ppgs/LJ_MFA/ADEPT_NO_VAD/middle/ --wav_dir /media/emelie/EMELIESSD1/puppeteering/LJ/ARPA/ADEPT_NO_VAD/no_reference/middle/
+python 1_compute_ctc_att_bnf.py --output_dir /media/emelie/EMELIESSD1/ppgs/LJ_MFA/ADEPT_NO_VAD/end/ --wav_dir /media/emelie/EMELIESSD1/puppeteering/LJ/ARPA/ADEPT_NO_VAD/no_reference/end/
+
+python 1_compute_ctc_att_bnf.py --output_dir /media/emelie/EMELIESSD1/ppgs/MELDJ_MFA/ADEPT_NO_VAD/beginning/ --wav_dir /media/emelie/EMELIESSD1/puppeteering/meLdJ/ARPA/ADEPT_NO_VAD/no_reference/beginning/
+python 1_compute_ctc_att_bnf.py --output_dir /media/emelie/EMELIESSD1/ppgs/MELDJ_MFA/ADEPT_NO_VAD/middle/ --wav_dir /media/emelie/EMELIESSD1/puppeteering/meLdJ/ARPA/ADEPT_NO_VAD/no_reference/middle/
+python 1_compute_ctc_att_bnf.py --output_dir /media/emelie/EMELIESSD1/ppgs/MELDJ_MFA/ADEPT_NO_VAD/end/ --wav_dir /media/emelie/EMELIESSD1/puppeteering/meLdJ/ARPA/ADEPT_NO_VAD/no_reference/end/
+
+# VAD
+python 1_compute_ctc_att_bnf.py --output_dir /media/emelie/EMELIESSD1/ppgs/LJ_MFA/ADEPT_VAD/beginning/ --wav_dir /media/emelie/EMELIESSD1/puppeteering/LJ/ARPA/ADEPT_VAD/no_reference/beginning/
+python 1_compute_ctc_att_bnf.py --output_dir /media/emelie/EMELIESSD1/ppgs/LJ_MFA/ADEPT_VAD/middle/ --wav_dir /media/emelie/EMELIESSD1/puppeteering/LJ/ARPA/ADEPT_VAD/no_reference/middle/
+python 1_compute_ctc_att_bnf.py --output_dir /media/emelie/EMELIESSD1/ppgs/LJ_MFA/ADEPT_VAD/end/ --wav_dir /media/emelie/EMELIESSD1/puppeteering/LJ/ARPA/ADEPT_VAD/no_reference/end/
+
+python 1_compute_ctc_att_bnf.py --output_dir /media/emelie/EMELIESSD1/ppgs/MELDJ_MFA/ADEPT_VAD/beginning/ --wav_dir /media/emelie/EMELIESSD1/puppeteering/meLdJ/ARPA/ADEPT_VAD/no_reference/beginning/
+python 1_compute_ctc_att_bnf.py --output_dir /media/emelie/EMELIESSD1/ppgs/MELDJ_MFA/ADEPT_VAD/middle/ --wav_dir /media/emelie/EMELIESSD1/puppeteering/meLdJ/ARPA/ADEPT_VAD/no_reference/middle/
+python 1_compute_ctc_att_bnf.py --output_dir /media/emelie/EMELIESSD1/ppgs/MELDJ_MFA/ADEPT_VAD/end/ --wav_dir /media/emelie/EMELIESSD1/puppeteering/meLdJ/ARPA/ADEPT_VAD/no_reference/end/
+
